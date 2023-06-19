887571372dab42530193618fc2da5e0c7cf6a1f1
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-06-19 12:54:27 -0600

    wip

--- src/app/services/marketplace.service.ts
+++ src/app/services/marketplace.service.ts
@@ -4,42 +4,76 @@ import {
   AbstractMarketplaceService,
   Marketplace,
   MarketplacePkg,
+  StoreIdentity,
   StoreInfo,
 } from '@start9labs/marketplace'
-import { combineLatest, filter, Observable, of, shareReplay } from 'rxjs'
-import { map, switchMap, tap } from 'rxjs/operators'
+import { combineLatest, filter, Observable, shareReplay } from 'rxjs'
+import {
+  distinctUntilChanged,
+  first,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs/operators'
 
 import { HOSTS } from '../tokens/hosts'
 import { UrlService } from './url.service'
+import { ActivatedRoute, Params } from '@angular/router'
 
 @Injectable()
 export class MarketplaceService extends AbstractMarketplaceService {
   private readonly http = inject(HttpClient)
   private readonly hosts = inject(HOSTS)
   private readonly url$ = inject(UrlService).getUrl$()
+  private readonly params$ = inject(QueryParams)
 
-  private readonly marketplace$: Observable<Marketplace> = combineLatest(
-    this.hosts.reduce(
-      (acc, { url }) => ({
-        ...acc,
-        [url]: combineLatest({
-          info: this.http.get<StoreInfo>(url + 'info'),
-          packages: this.http.get<MarketplacePkg[]>(
-            `${url}index?per-page=100&page=1`,
+  private readonly hosts$ = this.params$.pipe(
+    tap(params => console.error(params)), // always empty
+    map(params => {
+      const registry = params['api']
+      const name = params['name']
+      if (registry && name) {
+        return [
+          {
+            url: `https://${registry}/package/v0/`,
+            name,
+          },
+        ] as StoreIdentity[]
+      } else {
+        return this.hosts
+      }
+    }),
+  )
+
+  private readonly marketplace$: Observable<Marketplace> = this.hosts$
+    .pipe(
+      switchMap(hosts =>
+        combineLatest(
+          hosts.reduce(
+            (acc, { url }) => ({
+              ...acc,
+              [url]: combineLatest({
+                info: this.http.get<StoreInfo>(url + 'info'),
+                packages: this.http.get<MarketplacePkg[]>(
+                  `${url}index?per-page=100&page=1`,
+                ),
+              }),
+            }),
+            {},
           ),
-        }),
-      }),
-      {},
-    ),
-  ).pipe(shareReplay(1))
+        ),
+      ),
+      first(),
+    )
+    .pipe(shareReplay(1))
 
   getKnownHosts$() {
-    return of(this.hosts)
+    return this.hosts$
   }
 
   getSelectedHost$() {
-    return this.url$.pipe(
-      map(url => this.hosts.find(host => host.url === url)),
+    return combineLatest([this.url$, this.hosts$]).pipe(
+      map(([url, hosts]) => hosts.find(host => host.url === url)),
       filter(Boolean),
     )
   }
@@ -95,5 +129,12 @@ export class MarketplaceService extends AbstractMarketplaceService {
         this.http.get(`${url}${type}/${id}`, { responseType: 'text' }),
       ),
     )
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+class QueryParams extends Observable<Params> {
+  constructor({ queryParams }: ActivatedRoute) {
+    super(subscriber => queryParams.subscribe(subscriber))
   }
 }
