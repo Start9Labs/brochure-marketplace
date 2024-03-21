c61e52e9b6564250ef375eb22ebcc7ee2f7afa80
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2024-03-21 09:29:58 -0400

    fix icon

--- src/app/marketplace-menu/marketplace-menu.module.ts
+++ src/app/marketplace-menu/marketplace-menu.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core'
 import { MarketplaceMenuComponent } from './marketplace-menu.component'
 import { MenuModule } from '@start9labs/marketplace'
 import { TuiButtonModule } from '@taiga-ui/core'
+import { TuiAppearanceModule, TuiIconModule } from '@taiga-ui/experimental'
 
 @NgModule({
-  imports: [MenuModule, TuiButtonModule],
+  imports: [MenuModule, TuiButtonModule, TuiIconModule, TuiAppearanceModule],
   exports: [MarketplaceMenuComponent],
   declarations: [MarketplaceMenuComponent],
 })
